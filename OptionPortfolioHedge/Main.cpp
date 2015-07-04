debd77162a4f975c69dc1cdc90c0bd0ab8fe9b18
fbristotti <fbristotti@gmail.com>
2015-07-04 19:19:21 -0300

    ajustes para rodar em batch, ainda tem problema de memory leak

--- OptionPortfolioHedge/Main.cpp
+++ OptionPortfolioHedge/Main.cpp
@@ -3,6 +3,9 @@
 #include <math.h>
 #include <omp.h>
 #include <time.h>
+#include <iostream>
+#include <fstream>
+
 
 #ifndef Pi 
 #define Pi 3.141592653589793238462643 
@@ -24,8 +27,6 @@ float rnorm() {
 	return u * c;
 }
 
-float* B = NULL;
-
 // Método que retorna um vetor com um movimento browniano.
 float* MB(size_t n, float T) {
 
@@ -36,8 +37,7 @@ float* MB(size_t n, float T) {
 	float sh = sqrt(h);
 	float r;
 
-	if (B == NULL)
-		B = (float *)calloc(n + 1, sizeof(float));
+	float* B = (float *)calloc(n + 1, sizeof(float));
 
 	size_t i;
 	for (i = 0; i < n; i++)
@@ -110,6 +110,7 @@ float preco_bs(float T, float t, float S0, float K, float r, float sigma, option
 		return 0.0;
 		break;
 	}
+	free(d);
 }
 
 // Gera uma simulação da evolução do preço do ativo, já descontado na medida física.
@@ -122,6 +123,8 @@ void evolucao_real_bs(float T, float S0, float r, float sigma, size_t n, float m
 	{
 		S[i] = S0 * exp((mu - r - sigma*sigma / 2) * Tempos[i] + sigma * W[i]);
 	}
+
+	free(W);
 }
 
 // Cálculo da quantidade do ativo objeto da opção para realizar o delta hedging.
@@ -141,7 +144,7 @@ float hedging_bs(float T, float t, float S0, float K, float r, float sigma, opti
 		return 0.0;
 		break;
 	}
-
+	free(d);
 }
 
 // Cálculo do payoff da opção trazido a valor presente.
@@ -152,10 +155,10 @@ float payoff_descontado(float T, float r, float ST, float K, optionType opcao) {
 	switch (opcao)
 	{
 	case otEuroCall:
-		payoff = ST - K * exp(-r * T); // checar formula
+		payoff = ST - K * exp(-r * T);
 		break;
 	case otEuroPut:
-		payoff = K * exp(-r * T) - ST; // checar formula
+		payoff = K * exp(-r * T) - ST;
 		break;
 	default:
 		break;
@@ -180,27 +183,37 @@ size_t which(float* f, size_t n, float x) {
 	return i;
 }
 
-//struct HedingErrorParameters{
-//	float T
-//};
-//
-//float CalculaHedgingError(){
-//
-//}
+struct HedingErrorParameters{
+	float T; // Vencimento da opção
+	size_t M; // Número de simulações
+	size_t n; // Quantidade de intervalos gerados no tempo T para realizar a discretização
+	float mu; // taxa média de retorno
+	float S0; // Preço inicial da ação 
+	float r; // taxa de juros livre de risco
+	float sigma; // volatilidade BS
+	optionType opcao; // tipo de opção
+	float K; // strike da opção
+	size_t l; // quantidade de rebalanceamentos de delta hedging que será realizado no portfolio.
+};
 
-int main(int argc, char *argv[]){
+struct HedingErrorResult{
+	float error;
+	double elapsedTime;
+};
+
+HedingErrorResult CalculaHedgingError(HedingErrorParameters p){
 	double t1 = omp_get_wtime();
 
-	float T = 1.0; // Vencimento da opção
-	size_t M = 100000; // Número de simulações
-	size_t n = 50; // Quantidade de intervalos gerados no tempo T para realizar a discretização
-	float mu = 0.01;  // taxa média de retorno
-	float S0 = 100; // Preço inicial da ação 
-	float r = 0; // taxa de juros livre de risco
-	float sigma = 0.09; // volatilidade BS
-	optionType opcao = otEuroCall; // tipo de opção
-	float K = 100; // strike da opção
-	size_t l = 22; // quantidade de rebalanceamentos de delta hedging que será realizado no portfolio.
+	float T = p.T;
+	size_t M = p.M;
+	size_t n = p.n;
+	float mu = p.mu;
+	float S0 = p.S0;
+	float r = p.r;
+	float sigma = p.sigma;
+	optionType opcao = p.opcao;
+	float K = p.K;
+	size_t l = p.l;
 
 	float *erro = (float*)calloc(M, sizeof(float));
 	float *Tempos = seq(0.0, T, n);
@@ -221,7 +234,7 @@ int main(int argc, char *argv[]){
 		float *hedging = (float*)calloc(l, sizeof(float));
 		float Pay_descontado_real = 0;
 		float integral = 0, valor_portfolio_final = 0;
-		
+
 		// Gera uma trajetória do preço da ação que será utilizada como a trajetória real para cálculo do resultado da carteira.
 		evolucao_real_bs(T, S0, r, sigma, n, mu, Tempos, S);
 		Pay_descontado_real = payoff_descontado(T, r, S[n - 1], K, opcao);
@@ -249,15 +262,46 @@ int main(int argc, char *argv[]){
 	for (size_t i = 0; i < M; i++)
 		hedging_error += (erro[i] / M);
 
-	printf("%f\n", hedging_error);
-
 	free(Tempos);
 	free(erro);
 	free(tempoDeHedging);
 
-	double t2 = omp_get_wtime();
-	printf("Time elapsed: %f\n", t2 - t1);
+	HedingErrorResult result;
+	result.error = hedging_error;
+	result.elapsedTime = omp_get_wtime() - t1;
+
+	return result;
+}
+
+int main(int argc, char *argv[]){
+	
+	HedingErrorParameters p;
+	HedingErrorResult r;
+
+	FILE *pfile;
+	fopen_s(&pfile, "output.txt", "w");
+	fprintf(pfile, "HedgingError,ElapsedTime\n");
+
+	p.T = 1;
+	p.M = 10000;
+	p.n = 200;
+	p.mu = 0.01;
+	p.S0 = 100;
+	p.r = 0;
+	p.sigma = 0.09;
+	p.opcao = otEuroCall;
+	p.K = 100;
+	p.l = 30;	
+
+	for (int i = 0; i < 50; i++){
+		r = CalculaHedgingError(p);
+		fprintf(pfile, "%f,%f\n", r.error, r.elapsedTime);
+	}
+
+	fclose(pfile);
 
 	printf("Press [ENTER] to exit.");
 	int c = fgetc(stdin);
+
+	return 0;
 }
